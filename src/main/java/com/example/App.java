90c96191ae8974daad52b6aab176d532820b3af5
AnkithaaR <ankithar2504@gmail.com>
2025-12-22 18:16:57 +0530

    ankitha

--- src/main/java/com/example/App.java
+++ src/main/java/com/example/App.java
@@ -3,7 +3,6 @@ package com.example;
 public class App {
     public static void main(String[] args) {
         System.out.println("Hello Jenkins Maven!");
-        System.out.println("Hello Jenkins Maven!");
 
     }
 }
